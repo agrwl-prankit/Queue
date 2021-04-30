0d8e24830aed0c822ca6d70c8e3571a3536ba41d
agrwl-prankit <agrwnlprankit77@gmail.com>
2021-05-01 01:25:55 +0530

    add dequeue(), peek() and size() for QueueUsingSLL

--- src/queueUsingSLL/QueueUsingSLL.java
+++ src/queueUsingSLL/QueueUsingSLL.java
@@ -15,6 +15,20 @@ public class QueueUsingSLL {
         System.out.println(data + " inserted successfully");
     }
 
+    public void dequeue(){
+        if (isempty()) System.out.println("Queue is already empty");
+        else {
+            int temp = head.getData();
+            head = head.getNext();
+            System.out.println(temp + " removed successfully");
+        }
+    }
+
+    public void peak(){
+        if (isempty()) System.out.println("Queue is empty");
+        else System.out.println("Peak element is: " + head.getData());
+    }
+
     public void show(){
         if (isempty()) System.out.println("Queue is empty");
         else {
@@ -25,6 +39,19 @@ public class QueueUsingSLL {
                 temp = temp.getNext();
             }
             System.out.println();
+        }
+    }
+
+    public void size(){
+        if (isempty()) System.out.println("Size of queue is: 0");
+        else {
+            int size = 0;
+            SLLNode temp = head;
+            while (temp != null){
+                size++;
+                temp = temp.getNext();
+            }
+            System.out.println("Size of queue is: " + size);
         }
     }
 
--- src/queueUsingSLL/QueueUsingSLLMain.java
+++ src/queueUsingSLL/QueueUsingSLLMain.java
@@ -4,12 +4,19 @@ public class QueueUsingSLLMain {
     public static void main(String[] args) {
         QueueUsingSLL queue = new QueueUsingSLL();
         queue.show();
-        queue.enqueue(2);
+        queue.peak();
         queue.enqueue(4);
         queue.enqueue(5);
         queue.enqueue(6);
         queue.enqueue(7);
         queue.enqueue(9);
+        queue.size();
         queue.show();
+        queue.peak();
+        queue.dequeue();
+        queue.dequeue();
+        queue.size();
+        queue.show();
+        queue.peak();
     }
 }
